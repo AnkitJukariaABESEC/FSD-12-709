673693dfc74195a212c86251b757c7c49dc800ba
Ankit Jukaria <ankit.jukaria@gmail.com>
2026-07-23 11:54:20 +0530

    initial commit

---
+++ lab1/hello.js
@@ -0,0 +1,13 @@
+// console.log("Ankit Jukaria")
+// Write a function to take two numbers and return their sum.
+
+function sum(a,b){
+    return a+b;
+}
+
+const add=(a+b)=>{
+    return a+b;
+};
+const add2=(a,b)=>a+b;
+
+console.log(sum(20,10));
