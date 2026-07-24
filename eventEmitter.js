6ee735810a0793f8f7f023972dbd651fffd0f8cc
Ankit Jukaria <ankit.jukaria@gmail.com>
2026-07-24 14:35:38 +0530

    updated

---
+++ eventEmitter.js
@@ -0,0 +1,16 @@
+const login=(name)=>{
+    console.log(`${name} logged in`);
+};
+
+const start=()=>{
+    console.log("The system has started");
+};
+const working=(name)=>{
+    console.log(`${name} add items to cart`);
+};
+const checkout=(name)=>{
+console.log(`${name} logged out`);
+};
+
+login("Ankit Jukaria");
+start();
--- lab1/hello.js
+++ lab1/hello.js
@@ -5,9 +5,11 @@ function sum(a,b){
     return a+b;
 }
 
-const add=(a+b)=>{
+const add=(a,b)=>{
     return a+b;
 };
 const add2=(a,b)=>a+b;
 
 console.log(sum(20,10));
+console.log(add(30,10));
+console.log(add2(10,10));
